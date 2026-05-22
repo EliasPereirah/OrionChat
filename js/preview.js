f12666dfbc8db3a302e7eaa06495f1ce4c52f436
EliasPereirah <16616409+EliasPereirah@users.noreply.github.com>
2026-05-22 09:18:39 -0300

    Code preview

--- js/preview.js
+++ js/preview.js
@@ -1,6 +1,6 @@
-let preview = localStorage.getItem('preview');
-if (preview) {
-    document.open();
-    document.write(preview);
-    document.close();
+const previewData = localStorage.getItem('preview');
+const iframe = document.getElementById('preview-iframe');
+if (previewData && iframe) {
+    iframe.srcdoc = previewData;
 }
+document.title = previewData.match("<title>(.*?)</title>")?.[1] ?? 'OrionChat';
